7d2e7b080f7f61030c4e211be67f1b4109e4680b
yhsol <yhsol1592@gmail.com>
2019-06-16 14:31:03 +0900

    add initial commit

---
+++ src/Styles/Theme.js
@@ -0,0 +1,18 @@
+const BOX_BORDER = "1px solid #A7A7A7";
+const BORDER_RADIUS = "3PX";
+
+export default {
+  backgroundColor: "#FAFAFA",
+  textColor: "#000000",
+  whiteColor: "#FFFFFF",
+  blackColor: "#262626",
+  greyColor: "#FAFAFA",
+  darkGreyColor: "#999",
+  blueColor: "#3897f0",
+  navyColor: "#003569",
+  borderRadius: "3px",
+  whiteBox: `${BOX_BORDER}; 
+            ${BORDER_RADIUS}; 
+            background-color: white;
+            `
+};
