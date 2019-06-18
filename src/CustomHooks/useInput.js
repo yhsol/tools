9a09bc8136903df832b028c5da5cd1b99e82166b
yhsol <yhsol1592@gmail.com>
2019-06-18 20:47:02 +0900

    add useInput

--- src/CustomHooks/useInput.js
+++ src/CustomHooks/useInput.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const useInput = () => {
   const [value, setValue] = useState("");
@@ -14,3 +14,5 @@ const useInput = () => {
 };
 
 export default useInput;
+
+// 해당 hook 을 쓸 때 hook 을 쓰는 input 내에 value 와 onChange 값을 지정해서 hook 의 value 와 onChange 를 읽을 수 있게 해야한다.
